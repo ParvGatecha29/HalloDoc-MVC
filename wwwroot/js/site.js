20ec506a5869962918d8e90ab0b4229ef31b12f4
Parv Gatecha <tatva.dotnet.parvgatecha@outlook.com>
2024-02-02 17:46:30 +0530

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const btn1 = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+    document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+    document.body.classList.toggle("light-theme");
+}
+
+btn1.addEventListener("click", function () {
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+        var theme = document.body.classList.contains("light-theme")
+            ? "light"
+            : "dark";
+    } else {
+        document.body.classList.toggle("dark-theme");
+        var theme = document.body.classList.contains("dark-theme")
+            ? "dark"
+            : "light";
+    }
+    localStorage.setItem("theme", theme);
+});
+
+
+function upload(file){
+    var name = file.files[0].name;
+    var span = document.getElementById("upload");
+    span.innerHTML = name;
+} 
+
+function pass() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+
+    var e = document.querySelector("#eye");
+    e.classList.toggle("bi-eye-slash");
+    e.classList.toggle("bi-eye");
+}
