ec329cf943e5d5a394e754a0f2139a78499a1311
Parv Gatecha <tatva.dotnet.parvgatecha@outlook.com>
2024-02-26 18:23:12 +0530

    view case integration

--- Controllers/AdminDashboardController.cs
+++ Controllers/AdminDashboardController.cs
@@ -8,7 +8,12 @@ namespace HalloDoc.Controllers;
 public class AdminDashboardController : Controller
 {
     private readonly IAdminDashboardService _adminDashboardService;
-
+    int[] newcase = { 1 };
+    int[] pendingcase = { 2 };
+    int[] activecase = { 4,5 };
+    int[] concludecase = { 6 };
+    int[] toclosecase = { 3,7,8 };
+    int[] unpaidcase = { 9 };
     public AdminDashboardController(IAdminDashboardService adminDashboardService)
     {
         _adminDashboardService = adminDashboardService;
@@ -21,32 +26,42 @@ public class AdminDashboardController : Controller
     }
     public IActionResult NewCasePartial()
     {
-        var dash = _adminDashboardService.GetRequests();
-        return PartialView("_NewCase",dash);
+        var dash = _adminDashboardService.GetRequestsByStatus(newcase);
+        return PartialView("_CaseTable",dash);
     }
     public IActionResult PendingCasePartial()
     {
-        var dash = _adminDashboardService.GetRequests();
-        return PartialView("_PendingCase", dash);
+        var dash = _adminDashboardService.GetRequestsByStatus(pendingcase);
+        return PartialView("_CaseTable", dash);
     }
     public IActionResult ActiveCasePartial()
     {
-        var dash = _adminDashboardService.GetRequests();
-        return PartialView("_ActiveCase", dash);
+        var dash = _adminDashboardService.GetRequestsByStatus(activecase);
+        return PartialView("_CaseTable", dash);
     }
     public IActionResult ConcludeCasePartial()
     {
-        var dash = _adminDashboardService.GetRequests();
-        return PartialView("_ConcludeCase", dash);
+        var dash = _adminDashboardService.GetRequestsByStatus(concludecase);
+        return PartialView("_CaseTable", dash);
     }
     public IActionResult ToCloseCasePartial()
     {
-        var dash = _adminDashboardService.GetRequests();
-        return PartialView("_ToCloseCase", dash);
+        var dash = _adminDashboardService.GetRequestsByStatus(toclosecase);
+        return PartialView("_CaseTable", dash);
     }
     public IActionResult UnpaidCasePartial()
     {
-        var dash = _adminDashboardService.GetRequests();
-        return PartialView("_UnpaidCase", dash);
+        var dash = _adminDashboardService.GetRequestsByStatus(unpaidcase);
+        return PartialView("_CaseTable", dash);
+    }
+    public IActionResult ViewCase(int requestid)
+    {
+        var dash = _adminDashboardService.GetRequestById(requestid);
+        return View("ViewCase", dash);
+    }
+    public IActionResult ViewNotes(int requestid)
+    {
+        var dash = _adminDashboardService.GetRequestById(requestid);
+        return View("ViewNotes", dash);
     }
 }
